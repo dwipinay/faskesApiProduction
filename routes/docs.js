39d63f9c6f91a82741ca622dfb7679e924466e4f
dp <dp@MacBook-Air-dwi.local>
2023-04-27 21:47:48 +0700

    include json file

--- routes/docs.js
+++ routes/docs.js
@@ -1,8 +1,15 @@
 import express from 'express'
 const router = express.Router()
 
+import fs from "fs";
 import swaggerUi from 'swagger-ui-express'
-import apiDocs from '../documentations/apiDocs-1.json' assert { type: "json" };
+// import apiDocs from '../documentations/apiDocs-1.json' assert { type: "json" };
+
+const apiDocs = JSON.parse(
+    fs.readFileSync(new URL("../documentations/apiDocs-1.json", import.meta.url), {
+        encoding: "utf-8",
+    })
+)
 
 // DTO-PUSDATIN
 router.use('/faskes/apidocs-1-dto', swaggerUi.serve, (req, res) => {
